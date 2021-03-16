47c39a1a8d23243048ed0cdd975abfb5f298c88e
Ulrich <ulrich.baumeister@me.com>
2021-03-16 17:21:41 +0100

    Add Articles Listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 Rails.application.routes.draw do
   root 'alphablog#index'
   get 'about', to: 'alphablog#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
   resources :bookings, only: [:show]
 end
