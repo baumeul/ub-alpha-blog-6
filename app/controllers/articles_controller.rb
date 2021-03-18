4f594f0c1ee1b271269ef93a04f0304c707c6715
Ulrich <ulrich.baumeister@me.com>
2021-03-18 11:31:47 +0100

    Edit Record

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
     # render plain: params[:article]
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     # render plain: params[:article]
     @article = Article.new(params.require(:article).permit(:title, :description, :created_by, :updated_by))
@@ -21,6 +25,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description, :created_by, :updated_by))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
