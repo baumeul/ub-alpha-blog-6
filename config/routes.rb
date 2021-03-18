87ebed3c2b707bb575ae8319c62d9ccbb933a1c0
Ulrich <ulrich.baumeister@me.com>
2021-03-18 19:03:24 +0100

    CRUD fertig

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 Rails.application.routes.draw do
   root 'alphablog#index'
   get 'about', to: 'alphablog#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
   resources :bookings, only: [:show]
 end
