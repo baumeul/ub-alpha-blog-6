df5608277acb5ec7dc00aa7a4cd66157b8592048
Ulrich <ulrich.baumeister@me.com>
2021-03-18 11:05:05 +0100

    Flash Messages, Error Messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,21 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  def new
+    @article = Article.new
+    # render plain: params[:article]
+  end
+  
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description, :created_by, :updated_by))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 Rails.application.routes.draw do
   root 'alphablog#index'
   get 'about', to: 'alphablog#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
   resources :bookings, only: [:show]
 end
